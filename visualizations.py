2f5007927a93c05a46d8cd5560dad5516b059c9e
Lucy <lucywingard97@gmail.com>
2025-05-27 16:15:33 +0100

    built actual plots.

--- visualizations.py
+++ visualizations.py
@@ -12,6 +12,7 @@ import tqdm
 import tree
 
 import pickle
+import matplotlib.pyplot as plt
 
 from neural_networks_solomonoff_induction.data import data_generator as dg_lib
 from neural_networks_solomonoff_induction.data import utm_data_generator as utm_dg_lib
@@ -22,54 +23,61 @@ alphabet_size = 2
 probabilistic = True
 length = 256
 
+generate_fresh_batch = False
+visualize = True
+
 rng = np.random.default_rng(seed=1)
 
-program_sampler = utms_lib.MCSampler(
-    rng=rng,
-    filename='data/ctx_filtered_binary_probabilistic.pyd', # possibly data/ is necessary
-)
-# for i in range(10):
-#     p = program_sampler.sample_program(length)
-#     print(p)
+if generate_fresh_batch:
+    program_sampler = utms_lib.MCSampler(
+        rng=rng,
+        filename='data/ctx_filtered_binary_probabilistic.pyd', # possibly data/ is necessary
+    )
+    # for i in range(10):
+    #     p = program_sampler.sample_program(length)
+    #     print(p)
 
-utm = utms_lib.BrainPhoqueUTM(
-    program_sampler,
-    alphabet_size = alphabet_size,
-    print_trace = False,
-    shorten_program = True,
-    use_input_instruction = probabilistic,
-)
-data_generator = utm_dg_lib.UTMDataGenerator(
-    batch_size=10000,
-    seq_length=256,
-    rng=rng,
-    utm=utm,
-    memory_size=200, # matches optimize_Q
-    maximum_steps=1024, # matches optimize_Q
-    tokenizer=utm_dg_lib.Tokenizer.SEQ_POSITION,
-    maximum_program_length=256,
-)
+    utm = utms_lib.BrainPhoqueUTM(
+        program_sampler,
+        alphabet_size = alphabet_size,
+        print_trace = False,
+        shorten_program = True,
+        use_input_instruction = probabilistic,
+    )
+    data_generator = utm_dg_lib.UTMDataGenerator(
+        batch_size=10000,
+        seq_length=256,
+        rng=rng,
+        utm=utm,
+        memory_size=200, # matches optimize_Q
+        maximum_steps=1024, # matches optimize_Q
+        tokenizer=utm_dg_lib.Tokenizer.SEQ_POSITION,
+        maximum_program_length=256,
+    )
 
-batch, log_dict = data_generator.sample()
-batch = np.array(batch)
+    batch, log_dict = data_generator.sample()
+    batch = np.array(batch)
 
-print(batch.shape)
-binary_batch = batch.argmax(axis=-1)
-print(binary_batch.shape)
-#print(f"{binary_batch=}")
-# for row in binary_batch:
-#     print(row[:30])
-#print(log_dict)
+    print(batch.shape)
+    binary_batch = batch.argmax(axis=-1)
+    print(binary_batch.shape)
+    #print(f"{binary_batch=}")
+    # for row in binary_batch:
+    #     print(row[:30])
+    #print(log_dict)
 
-with open('binary_batch_log_dict.pkl', 'wb') as f:
-    pickle.dump((binary_batch,log_dict),f)
+    with open('binary_batch_log_dict.pkl', 'wb') as f:
+        pickle.dump((binary_batch,log_dict),f)
 
-if False:
+with open('binary_batch_log_dict.pkl', 'rb') as f:
+    binary_batch, log_dict = pickle.load(f)
+
+if visualize:
     config = transformer.TransformerConfig(vocab_size=alphabet_size)
     model = hk.transform(
         functools.partial(transformer.transformer_decoder, config=config)
     )
-    fname = "params.npz"
+    fname = "params_mid_train.npz"
     with open(fname, "rb") as f:
         loaded_params = np.load(f, allow_pickle=True)
         params = dict(loaded_params)
@@ -85,4 +93,9 @@ if False:
         conditionals, binary_batch[..., None], axis=-1
     )[..., 0]
     print(true_conditionals)
+    cond_probs = true_conditionals.exp()
+
+    plt.plot(true_conditionals.mean(axis=0))
+    plt.plot(cond_probs.mean(0))
+
     # still need to handle masking, see _make_loss_fn
